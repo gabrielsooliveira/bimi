3d9ecef08100962a20505ae2129edfba894c11c9
Gabriel Souza <gabrielsouzacontato.99@gmail.com>
2024-10-16 07:52:25 -0300

    alteração no authenticate

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -13,7 +13,17 @@ class Authenticate extends Middleware
     protected function redirectTo(Request $request): ?string
     {
         if (!$request->expectsJson()) {
+            if ($request->is('energy/*') || $request->is('energy')) {
+                return route('energy.login');
+            }
 
+            if ($request->is('phone/*') || $request->is('phone')) {
+                return route('phone.login');
+            }
+
+            if ($request->is('health/*') || $request->is('health')) {
+                return route('health.login');
+            }
         }
 
         return null;
